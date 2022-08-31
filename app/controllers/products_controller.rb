3097ef1248842cb3d492305f960b0354e033a047
Kade Illian <kadeillianmt@gmail.com>
2022-08-31 11:47:09 -0600

    added a way to use 'params' in your url to select a product by its id

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -4,28 +4,8 @@ class ProductsController < ApplicationController
     render json: products.as_json
   end
 
-  def display_sling_fin_portal
-    sling_fin_portal = Product.find_by(id: 1)
-    render json: sling_fin_portal.as_json
-  end
-
-  def display_rei_magma
-    rei_magma = Product.find_by(id: 2)
-    render json: rei_magma.as_json
-  end
-
-  def display_therma_rest
-    therma_rest = Product.find_by(id: 3)
-    render json: therma_rest.as_json
-  end
-
-  def display_jetboil_minimo
-    jetboil_minimo = Product.find_by(id: 4)
-    render json: jetboil_minimo.as_json
-  end
-
-  def display_kuiu_trekking_poles
-    kuiu_trekking_poles = Product.find_by(id: 5)
-    render json: kuiu_trekking_poles.as_json
+  def one_product
+    one_product = Product.find_by(id: params["id"])
+    render json: one_product.as_json
   end
 end

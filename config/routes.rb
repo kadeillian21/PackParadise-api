795d03a494da2ec331784345aff928d000420565
Kade Illian <kadeillianmt@gmail.com>
2022-08-31 10:50:23 -0600

    created individual routes for each product

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,9 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
   get "/products", controller: "products", action: "list_products"
+  get "/sling_fin_portal", controller: "products", action: "display_sling_fin_portal"
+  get "/rei_magma", controller: "products", action: "display_rei_magma"
+  get "/therma_rest", controller: "products", action: "display_therma_rest"
+  get "/jetboil_minimo", controller: "products", action: "display_jetboil_minimo"
+  get "/kuiu_trekking_poles", controller: "products", action: "display_kuiu_trekking_poles"
 end

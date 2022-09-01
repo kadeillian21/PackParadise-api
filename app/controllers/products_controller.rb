c974c94059d1846529064a19814d05a0a294113a
Kade Illian <kadeillianmt@gmail.com>
2022-09-01 11:03:48 -0600

    added a destroy method and delete route.  Updated database to show orignial 5 items with the 5th being updated

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -30,4 +30,11 @@ class ProductsController < ApplicationController
     product.save
     render json: product.as_json
   end
+
+  def destroy
+    product = Product.find_by(id: params["id"])
+    product.destroy
+
+    render json: { message: "The product has been successfully deleted!" }
+  end
 end

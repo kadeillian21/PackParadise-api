df41548ef9cd6e4517530f029cab57121b4358cf
Kade Illian <kadeillianmt@gmail.com>
2022-09-06 10:50:09 -0600

    added bad paths in our products controller.  Fully functioning validation.. commented out image validation because it only looks for png jpb and jpeg... does not take in linked images

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,8 @@
 class Product < ApplicationRecord
-  validates :name, presence: true
+  validates :name, presence: true, uniqueness: true
+  validates :price, presence: true, numericality: { greater_than_or_equal_to: 0 }
+  validates :description, length: { in: 10..500 }
+  # validates_format_of :image_url, :with => %r{\.(png|jpb|jpeg)$}i, :message -> "Must have vaild image file type", :multiline => true
 
   def is_discounted?
     price < 10
